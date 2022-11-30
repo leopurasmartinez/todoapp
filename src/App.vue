4e3aa3e01ca3559d2b91a0078a3783e360e31eeb
leo <leopurasmartinez@gmail.com>
2022-11-30 20:33:51 +0100

    Load store on app creation

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@
   </div>
 </template>
 
+<script>
+  export default {
+    beforeCreate() {
+      this.$store.commit('loadStore');
+    }
+  }
+</script>
+
 <style>
 
 @import './assets/stylesheets/index.css';

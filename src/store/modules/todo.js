55000462c335be08e6450f378eca8e113c4a05e0
leo <leopurasmartinez@gmail.com>
2022-12-05 15:50:05 +0100

    Removed duplicated store mutation

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -13,17 +13,6 @@ const todo = {
     }
   },
   mutations: {
-    loadStore() {
-      if (localStorage.getItem('store')) {
-        try {
-          this.replaceState(JSON.parse(localStorage.getItem('store')));
-        }
-        catch(e) {
-          console.log('Could not initialize store', e);
-        }
-      }
-    },
-
     addTodo (state, todoItem) {
       state.todos.push(todoItem)
     },
